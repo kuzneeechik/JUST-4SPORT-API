7285e90ee2fd27920927ada7ba72a280dbc148f2
Vasilina Kuznetsova <vasilina.kuznecova2@gmail.com>
2026-04-28 00:16:04 +0700

    Initial commit

---
+++ src/main/java/ru/hits/just_4sport/Just4sportApplication.java
@@ -0,0 +1,13 @@
+package ru.hits.just_4sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Just4sportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Just4sportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hits/just_4sport/Just4sportApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hits.just_4sport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Just4sportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
