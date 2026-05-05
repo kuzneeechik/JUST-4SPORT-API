1ef206739c9e4d317284a8f325f86da191c1cddd
Vasilina Kuznetsova <vasilina.kuznecova2@gmail.com>
2026-05-05 23:17:33 +0700

    Add custom user details service

---
+++ src/main/java/ru/hits/just_4sport/UserRepository.java
@@ -0,0 +1,11 @@
+package ru.hits.just_4sport;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.hits.just_4sport.model.domain.UserEntity;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByEmail(String email);
+}
---
+++ src/main/java/ru/hits/just_4sport/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package ru.hits.just_4sport.service;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.hits.just_4sport.UserRepository;
+import ru.hits.just_4sport.model.domain.UserEntity;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public @NonNull UserDetails loadUserByUsername(@NonNull String email) {
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+
+        return User.builder()
+                .username(user.getEmail())
+                .password(user.getPasswordHash())
+                .authorities(List.of())
+                .build();
+    }
+}
