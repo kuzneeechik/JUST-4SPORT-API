e6576c9b563133a954a5d42a2b09473906638622
Vasilina Kuznetsova <vasilina.kuznecova2@gmail.com>
2026-06-03 21:11:03 +0700

    add catching validation exception to exception handler

--- src/main/java/ru/hits/just_4sport/infrastructure/CustomExceptionHandler.java
+++ src/main/java/ru/hits/just_4sport/infrastructure/CustomExceptionHandler.java
@@ -2,11 +2,14 @@ package ru.hits.just_4sport.infrastructure;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.hits.just_4sport.infrastructure.exception.BadRequestException;
 import ru.hits.just_4sport.infrastructure.exception.NotFoundException;
 import ru.hits.just_4sport.model.api.ExceptionResponseModel;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -20,6 +23,18 @@ public class CustomExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ExceptionResponseModel> catchBadRequestException(BadRequestException exception) {
         return new ResponseEntity<>(new ExceptionResponseModel(HttpStatus.BAD_REQUEST.value(), exception.getMessage()),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionResponseModel> catchValidationException(MethodArgumentNotValidException exception) {
+        var message = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+
+        return new ResponseEntity<>(new ExceptionResponseModel(HttpStatus.BAD_REQUEST.value(), message),
                 HttpStatus.BAD_REQUEST);
     }
 }
